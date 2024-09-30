1e3d3ce64e92377ad8d8e6643dc8c10a0f50ca12
LauraPou <laura.ivanna.zamora.montoya@gmail.com>
2024-09-29 20:46:19 -0600

    se agregó función liberarCasilla

--- Buscaminas.java
+++ Buscaminas.java
@@ -4,16 +4,71 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Buscaminas {
     public static void main (String args[]){
         Scanner entrada = new Scanner(System.in);
-        int n;
-
-        // Nivel principiante: tablero de 9 × 9 casillas y 10 minas. 
+        int nivel;
         System.out.println("Ingresa el nivel del juego:\n1 Principiante\n2 Intermedio\n3 Experto");
-        n = entrada.nextInt();
-        char[][] matriz =generarMatriz(n);
-        imprimirMatriz(matriz);
-        
+        nivel = entrada.nextInt();
+        iniciarJuego(nivel);
     }
 
+    public static void iniciarJuego(int nivel){
+        Scanner entrada = new Scanner(System.in);
+        int fila=0, columna=0, movimiento;
+        //  se genera una matriz solo para mostrar las casillas tapadas al inicio
+        char[][] matrizCliente = generarMatriz(nivel, fila, columna);
+        for (int i = 0; i < matrizCliente.length; i++) {
+            for (int j = 0; j < matrizCliente.length; j++) {
+                matrizCliente[i][j]='-';
+            }
+        }
+        imprimirMatriz(matrizCliente);
+        System.out.println("Indica la coordenada inicial: (fila) (columna), ejemplo: 0 2 (columna=0, fila=2)");
+        fila = entrada.nextInt();
+        columna = entrada.nextInt();
+        char[][] matrizServidor = generarMatriz(nivel, fila, columna);
+        imprimirMatriz(matrizServidor);
+        matrizCliente = liberarCasilla(matrizServidor, matrizCliente, fila, columna);
+        imprimirMatriz(matrizCliente);
+        System.out.println("¿Qué quieres hacer?\n1 Poner bandera\n2 Quitar bandera\n3 Abrir casilla");
+        movimiento = entrada.nextInt();
+        System.out.println("Indica la coordenada: (fila) (columna)");
+        fila = entrada.nextInt();
+        columna = entrada.nextInt();
+        switch (movimiento) {
+            case 1:
+                
+                break;
+            case 2:
+                break;
+            case 3:
+                matrizCliente = liberarCasilla(matrizServidor, matrizCliente, fila, columna);
+                imprimirMatriz(matrizCliente);
+                break;
+        
+            default:
+                break;
+        }
+        
+        
+
+
+    }
+
+    public static char[][] liberarCasilla(char[][] matrizServidor,char[][] matrizCliente, int fila, int columna){
+        int[] x = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+        int[] y = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+        if(matrizCliente[fila][columna]!='-'){return matrizCliente;}
+        // se libera la casilla
+        matrizCliente[fila][columna] = matrizServidor[fila][columna];
+        if (matrizServidor[fila][columna] != '0') {return matrizCliente;}
+        for(int i = 0; i < x.length; i++) {
+            int nuevaFila = fila + x[i];
+            int nuevaColumna = columna + y[i];
+            if (nuevaFila >= 0 && nuevaFila < matrizServidor.length && nuevaColumna >= 0 && nuevaColumna < matrizServidor[0].length) {
+                liberarCasilla(matrizServidor, matrizCliente, nuevaFila, nuevaColumna);
+            }
+        }
+        return matrizCliente;
+    }
 
     public static void imprimirMatriz(char[][] matriz){
         System.out.println("-------------------------------------------\n");
@@ -25,7 +80,7 @@ public class Buscaminas {
         }
     }
 
-    public static char[][] generarMatriz(int nivel){
+    public static char[][] generarMatriz(int nivel, int fila, int columna){
         int minas = 0, n = 0, m = 0, c = 0;
         if(nivel == 1){ // principiante
             minas = 10;
@@ -41,35 +96,30 @@ public class Buscaminas {
             m = 30;
         }
         char[][] matriz = new char[n][m];
-
         // primero llenar con ceros la matriz
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 matriz[i][j] = '0';
             }
         }
-        imprimirMatriz(matriz);
         // posicionar las minas en coordenadas aleatorias
         int X,Y;
         while ( c < minas) {
             X = ThreadLocalRandom.current().nextInt(0, n);
             Y = ThreadLocalRandom.current().nextInt(0, m);
-            if( matriz[X][Y] != 'M' ){
+            if( matriz[X][Y] != 'M' && X!=fila && Y!=columna ){
                 matriz[X][Y] = 'M';
                 c++;
             }
         }
-        imprimirMatriz(matriz);
-        int nMinas=0;
         // poner adyacencias
         int[] x = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
         int[] y = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                     if(matriz[i][j] == 'M'){
-                        nMinas++;
                         for (int k = 0; k < y.length; k++) {
-                            if( x[k]+i >= 0 &&  j+y[k] >= 0 && x[k]+i < 9 && j+y[k] < 9 && matriz[x[k]+i][y[k]+j] != 'M'){
+                            if( x[k]+i >= 0 &&  j+y[k] >= 0 && x[k]+i < n && j+y[k] < m && matriz[x[k]+i][y[k]+j] != 'M'){
                                 matriz[x[k]+i][y[k]+j]++;
                         }
                     }
@@ -77,10 +127,10 @@ public class Buscaminas {
 
             }
         }
-        System.out.println("minas encontradas: "+ nMinas);
-        
         return matriz;
     }
+
+    
 
 }
 
